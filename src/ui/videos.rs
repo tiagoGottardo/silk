12025715acfe9a898b74722dc2878b1aab90ba72
tiagoGottardo <tiago.panizio@hotmail.com>
2025-05-31 14:24:52 -0300

    refactor: add function to download on menu

--- src/ui/videos.rs
+++ src/ui/videos.rs
@@ -10,18 +10,45 @@ type Terminal = ratatui::Terminal<CrosstermBackend<Stdout>>;
 use ratatui::{
     crossterm::event::{self, Event, KeyCode},
     prelude::CrosstermBackend,
-    style::{Color, Modifier, Style},
-    text::{Line, Span},
+    style::{Color, Style},
+    text::Line,
     widgets::{Block, Borders, Paragraph},
 };
-use tokio::task;
 
 use crate::{
     terminal,
-    youtube::download::{DownloadType, download_from_yt},
+    youtube::{
+        download::{DownloadType, download_from_yt},
+        parser::{ContentItem, VideoProps},
+    },
 };
 
-use crate::types::VideoInfo;
+async fn download_on_menu(
+    menu_items: Arc<Mutex<Vec<ContentItem>>>,
+    index: usize,
+    download_type: DownloadType,
+) {
+    let clone = Arc::clone(&menu_items);
+    if let ContentItem::Video(video_props) = &mut menu_items.lock().unwrap()[index] {
+        video_props.tag = match download_type {
+            DownloadType::Video => String::from("Downloading..."),
+            DownloadType::Audio => String::from("Downloading Audio..."),
+        };
+
+        let url = video_props.url.clone();
+
+        tokio::task::spawn(async move {
+            let result = match download_from_yt(&url, download_type).await {
+                Ok(_) => String::from("Downloaded!"),
+                Err(_) => String::from("Some error occur on dowload!"),
+            };
+
+            if let ContentItem::Video(video_props) = &mut clone.lock().unwrap()[index] {
+                video_props.tag = result;
+            }
+        });
+    }
+}
 
 pub async fn videos_interface(
     terminal: &mut Terminal,
@@ -58,52 +85,12 @@ pub async fn videos_interface(
             if let Event::Key(key) = event::read()? {
                 match key.code {
                     KeyCode::Char('d') => {
-                        let chosen = selected;
-
-                        {
-                            let mut items = menu_items.lock().unwrap();
-                            items[chosen].tag = String::from("Downloading...");
-                        }
-
-                        let menu_items_clone = Arc::clone(&menu_items);
-                        let url = {
-                            let items = menu_items.lock().unwrap();
-                            items[chosen].url.clone()
-                        };
-
-                        task::spawn(async move {
-                            let result = download_from_yt(&url, DownloadType::Video).await;
-
-                            let mut items = menu_items_clone.lock().unwrap();
-                            items[chosen].tag = match result {
-                                Ok(_) => String::from("Downloaded!"),
-                                Err(_) => String::from("Some error occurred on download."),
-                            };
-                        });
+                        download_on_menu(Arc::clone(&menu_items), selected, DownloadType::Video)
+                            .await;
                     }
                     KeyCode::Char('m') => {
-                        let chosen = selected;
-
-                        {
-                            let mut items = menu_items.lock().unwrap();
-                            items[chosen].tag = String::from("Downloading audio...");
-                        }
-
-                        let menu_items_clone = Arc::clone(&menu_items);
-                        let url = {
-                            let items = menu_items.lock().unwrap();
-                            items[chosen].url.clone()
-                        };
-
-                        task::spawn(async move {
-                            let result = download_from_yt(&url, DownloadType::Audio).await;
-
-                            let mut items = menu_items_clone.lock().unwrap();
-                            items[chosen].tag = match result {
-                                Ok(_) => String::from("Audio Downloaded!"),
-                                Err(_) => String::from("Some error occurred on download."),
-                            };
-                        });
+                        download_on_menu(Arc::clone(&menu_items), selected, DownloadType::Audio)
+                            .await;
                     }
                     KeyCode::Up | KeyCode::Char('k') => {
                         if selected > 0 {
@@ -120,7 +107,11 @@ pub async fn videos_interface(
                         return Ok(None);
                     }
                     KeyCode::Enter | KeyCode::Char('l') => {
-                        return Ok(Some(menu_items.lock().unwrap()[selected].clone()));
+                        if let ContentItem::Video(video) =
+                            menu_items.lock().unwrap()[selected].clone()
+                        {
+                            return Ok(Some(video));
+                        }
                     }
                     _ => {}
                 }
